e724fad902fb25579599c79d82304e0de98691e4
SathishGunasankaran <sathishgunasankaran@gmail.com>
2025-11-21 17:03:07 +0530

    Advanced DSA

---
+++ src/AdvancedDSA/Day1/SubArraySumApproach1.java
@@ -0,0 +1,21 @@
+package AdvancedDSA.Day1;
+
+public class SubArraySumApproach1 {
+    public static void main(String[] args) {
+//        int[] arr = {4, 5, 2, 1, 6};
+          int[] arr = {-4, -3, -6, -9, -2};
+
+        int ans = Integer.MIN_VALUE;
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                int sum = 0;
+                for(int k=i;k<=j;k++){
+                    sum +=  arr[k];
+                }
+                ans = Math.max(ans,sum);
+            }
+        }
+        System.out.println(ans);
+    }
+}
